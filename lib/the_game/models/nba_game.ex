826b3bd3913eb0e8c69f20286981d1b006359c07
Neil Scheuermann <neilscheuermann@gmail.com>
2021-04-15 07:26:15 -0600

    Adds team logos to NBA game struct and UI

--- lib/the_game/models/nba_game.ex
+++ lib/the_game/models/nba_game.ex
@@ -5,27 +5,41 @@ defmodule TheGame.NBAGame do
   defstruct [
     :clock,
     :current_period,
+    :h_team_logo_svg,
     :h_team_score,
     :h_team_tricode,
     :point_diff,
+    :v_team_logo_svg,
     :v_team_score,
     :v_team_tricode
   ]
 
   def format_game(game) do
     h_team_score = Map.get(game, "hTeam") |> Map.get("score")
+    h_team_team_id = Map.get(game, "hTeam") |> Map.get("teamId")
+    h_team_logo_svg = get_team_logo_svg(h_team_team_id)
+
     v_team_score = Map.get(game, "vTeam") |> Map.get("score")
+    v_team_team_id = Map.get(game, "vTeam") |> Map.get("teamId")
+    v_team_logo_svg = get_team_logo_svg(v_team_team_id)
+
     point_diff = get_point_diff(v_team_score, h_team_score)
 
     %TheGame.NBAGame{
       clock: Map.get(game, "clock"),
       current_period: Map.get(game, "period") |> Map.get("current"),
+      h_team_logo_svg: h_team_logo_svg,
       h_team_score: h_team_score,
       h_team_tricode: Map.get(game, "hTeam") |> Map.get("triCode"),
       point_diff: point_diff,
+      v_team_logo_svg: v_team_logo_svg,
       v_team_score: v_team_score,
       v_team_tricode: Map.get(game, "vTeam") |> Map.get("triCode")
     }
+  end
+
+  defp get_team_logo_svg(team_id) do
+    "https://cdn.nba.com/logos/nba/#{team_id}/primary/L/logo.svg"
   end
 
   defp get_point_diff(score_a, score_b) when score_a === "" or score_b === "" do
